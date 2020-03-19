df22594e8cd2758ab5147d325f8fad6cc8eb21ca
hex96 <58548001+hex96@users.noreply.github.com>
2020-03-19 20:36:10 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
-from setuptools import setup
+import os
 
-setup(
-    name='py_display',
-    version='1.0.0',
-    scripts=['display/__init__.py']
-)
+path = '/private/var/mobile/Containers/Shared/AppGroup/CC849E56-88FE-4C4A-93D9-A62A38FB2B80/Pythonista3/Documents/display/display'
+
+os.chdir(path)
+
+init_text = open('__init__.py', 'r').read()
+
+path = '/private/var/mobile/Containers/Shared/AppGroup/CC849E56-88FE-4C4A-93D9-A62A38FB2B80/Pythonista3/Documents/site-packages/display/'
+
+os.mkdir(path)
+os.chdir(path)
+
+file = open('__init__.py', 'w')
+file.write(init_text)
+file.close()
